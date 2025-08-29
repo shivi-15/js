7a374403f5ba7850c12809959ab2c9426bbacb33
Shivi Kumari <shivikumari1304.sk@gmail.com>
2025-08-29 13:04:06 +0000

    added data type file

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -2,4 +2,31 @@
 
 // alert(3+3) // we are using node.js , not browser
 
-console.log(3+3);
+console.log(3+3); // code should be readable 
+
+
+
+let nam = "Shivi";
+let age = 20;
+let isLoggedIn = false;
+let state = null ; 
+//undefiend matlb value abhi asign nae hue hai .. baaad me hogi
+
+//Primitive data typo
+// number = 2 to power  53
+// bigint 
+// boolean =  true / false 
+// null = standalone value 
+// undefined =
+// symbol = unique 
+
+//Non-primitive data type 
+// object
+
+console.log(typeof nam)
+console.log(typeof age)
+console.log(typeof(undefined)); // undefined 
+console.log(typeof null); // object .... type of null is object
+
+//MDN 
+//ECMAScript

1b607e98e2a8d7e5454e15197acda38e6af30553
Shivi Kumari <shivikumari1304.sk@gmail.com>
2025-08-29 19:24:31 +0000

    add 2 files

---
+++ 01_basics/03_coversion.js
@@ -0,0 +1,31 @@
+let score = 33 
+console.log(typeof score)
+
+// "33" = 33
+// "33abc" = NaN 
+// true = 1 ; false = 0 
+
+
+let a = "199ab"
+let b = Number(a)
+console.log(b)
+
+let c = String(a)
+console.log(c)
+
+// 1 = true , 0 = false 
+// "" = false
+// "shivi" = true 
+
+let fn = "Shivi"
+let bool = Boolean(fn)
+console.log(bool)
+
+let d = ""
+let bool2 = Boolean(d)
+console.log(bool2)
+
+
+
+
+
